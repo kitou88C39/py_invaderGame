2d3ce25993f9584dc2b7508465054ccd3b77f3d7
kitou88C39 <kanfutrooper@icloud.com>
2024-01-14 17:12:07 +0900

    Invaders game basics

--- sample.py
+++ sample.py
@@ -21,8 +21,8 @@ playerX_change = 0
 # 音声の出力
 # mixer.Sound("laser.wav").play()
 
-def play()
-    screen.bulit(img, (X,Y))
+def player(x, y):
+    screen.bulit(img, (X, Y))
 
 
 # 画面をずっと表示させる 無限ループ
