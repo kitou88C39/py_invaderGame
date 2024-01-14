bb57f7aa593cd3b82dfb061639452a1962bd73bc
kitou88C39 <kanfutrooper@icloud.com>
2024-01-14 14:21:33 +0900

    create a game

--- sample.py
+++ sample.py
@@ -6,12 +6,16 @@ pygame.init()
 # 画面を作成する
 
 screen = pygame.display.set_mode((800, 600))
+# Windowのタイトルを変更する
+pygame.display.set_caption("Invaders Game")
+
+
 
 # 画面をずっと表示させる 無限ループ
-
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+# タイトルの変更
